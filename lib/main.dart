44dcf5690dac41c3a57142d80243d52a960443d6
nag763 <loic.labeye@tutanota.de>
2021-03-06 16:44:14 +0100

    Listed card view elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webfeed/webfeed.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
@@ -15,13 +16,17 @@ class MyApp extends StatelessWidget {
     DateTime now = new DateTime.now();
     var formatter = new DateFormat('dd-MM-yyyy');
     String formattedDate = formatter.format(now);
-    feedUrl = Uri.https("webnext.fr", "templates/webnext_exclusive/views/includes/epg_cache/programme-tv-rss_"+formattedDate+".xml");
+    feedUrl = Uri.https(
+        "webnext.fr",
+        "templates/webnext_exclusive/views/includes/epg_cache/programme-tv-rss_" +
+            formattedDate +
+            ".xml");
     return MaterialApp(
-      title: 'Mon programme tv du '+formattedDate,
+      title: 'Mon programme tv du ' + formattedDate,
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Mon programme tv du '+formattedDate),
+      home: MyHomePage(title: 'Mon programme tv du ' + formattedDate),
     );
   }
 }
@@ -35,16 +40,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<RssItem> items;
+  int itemCount = 0;
 
-  var _content='Aucune information';
   Future<String> fetchRSS() async {
-    var response = await http.get(feedUrl);
-    var _rssFeed = RssFeed.parse(response.body);
+    var response = await http.get(feedUrl, headers:{'Content-Type':'charset=utf-8'});
+    var _rssFeed = RssFeed.parse(Utf8Decoder().convert(response.bodyBytes));
     setState(() {
-      _content = _rssFeed.items.map((e) => e.title).join("\n");
+      items = _rssFeed.items;
+      itemCount = _rssFeed.items.length;
     });
   }
-
 
   @override
   void initState() {
@@ -58,17 +64,35 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 50, 10, 10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             ListTile(
               leading: Icon(Icons.web),
               title: Text(widget.title),
             ),
-            Text(
-              '$_content',
-            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: itemCount,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(items[index].title),
+                          subtitle: Text(items[index].description),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
